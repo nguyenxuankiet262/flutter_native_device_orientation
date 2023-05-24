530d0ec981f51b44ca24b2d83a9fd192487784a1
OS.ITBee Nguyễn Xuân Kiệt <kiet.nguyen@itbeesolutions.vn>
2023-05-24 17:13:34 +0700

    test

--- ios/Classes/OrientationListener.m
+++ ios/Classes/OrientationListener.m
@@ -14,8 +14,18 @@
 }
 
 - (void)startOrientationListener:(void (^)(NSString* orientation)) orientationRetrieved {
-    UIDevice *device = [UIDevice currentDevice];
-    [device beginGeneratingDeviceOrientationNotifications];
+
+    if (@available(iOS 16.0, *)) {
+        NSArray *array = [[[UIApplication sharedApplication] connectedScenes] allObjects];
+        UIWindowScene *scene = (UIWindowScene *)array[0];
+        [UIViewController attemptRotationToDeviceOrientation];
+        UIWindowSceneGeometryPreferencesIOS *geometryPreferences = [[UIWindowSceneGeometryPreferencesIOS alloc] initWithInterfaceOrientations:maskOrientation];
+        [scene requestGeometryUpdateWithPreferences:geometryPreferences errorHandler:^(NSError * _Nonnull error) {}];
+    } else {
+        UIDevice *device = [UIDevice currentDevice];
+        [device beginGeneratingDeviceOrientationNotifications];
+    }
+
     NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
     self.observer = [nc addObserverForName:UIApplicationDidChangeStatusBarOrientationNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notification) {
 
@@ -57,8 +67,10 @@
 
 - (void)stopOrientationListener {
     NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-    UIDevice *device = [UIDevice currentDevice];
-    [device endGeneratingDeviceOrientationNotifications];
+    if (@available(iOS 16.0, *)) {
+        UIDevice *device = [UIDevice currentDevice];
+        [device endGeneratingDeviceOrientationNotifications];
+    }
     [nc removeObserver:self.observer];
     self.observer = NULL;
 }
